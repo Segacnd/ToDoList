58262169a149243017455e125ad172ec94d87dda
Segacnd <segabelka@yandex.ru>
2022-07-18 15:58:56 +0400

    create new color scrolllbar

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
 jQuery('#rangestart').calendar({
-    type: 'date',
-    endCalendar:jQuery('#rangeend')
-  });
-  jQuery('#rangeend').calendar({
-    type: 'date',
-    startCalendar:jQuery('#rangestart')
-  });
+  type: 'date',
+  endCalendar: jQuery('#rangeend')
+});
+jQuery('#rangeend').calendar({
+  type: 'date',
+  startCalendar: jQuery('#rangestart')
+});
 
 
-  
+
 //    jQuery('.ui.progress')
 //     .progress({
 //       duration : 200,
@@ -28,12 +28,12 @@ const COMPLETED_STATUS = 'complete';
 const toDo = {
   createNewTask: function (userTexst, taskId, total, value) {
     const taskTest = document.createElement('div')
-    
+
     const uiToggleCheckbox = document.createElement('div')
     const checkbox = document.createElement('input')
     const labelCB = document.createElement('label')
 
-    const taskTexst = document.createElement('div')  
+    const taskTexst = document.createElement('div')
 
     const uiIndicatingProgress = document.createElement('div')
     const bar = document.createElement('div')
@@ -49,7 +49,7 @@ const toDo = {
     checkbox.name = 'public';
     taskTexst.className = 'taskTexst';
     uiIndicatingProgress.className = 'ui indicating progress style';
-    uiIndicatingProgress.classList. add(taskId)
+    uiIndicatingProgress.classList.add(taskId)
     bar.className = 'bar';
     progress.className = 'progress';
     label.className = 'label';
@@ -77,7 +77,7 @@ const toDo = {
     if (this.currentStatus == ACTIVE_STATUS) {
       this.createProgressBar(value, total, taskId)
     }
-  
+
     checkbox.addEventListener('change', this.deleteTask.bind(this));
   },
 
@@ -97,7 +97,7 @@ const toDo = {
   },
 
   handleFormSubmit: function (event) {
-    event.preventDefault();   
+    event.preventDefault();
     // location.reload();
     const formData = new FormData(event.target);
 
@@ -105,152 +105,152 @@ const toDo = {
     const dateStart = formData.get("start");
     const dateEnd = formData.get("end");
 
-    
+
     const generatedId = this.generateId();
     const { total, value } = this.transformDate(dateStart, dateEnd);
 
-    if(userTexst.length && dateStart && dateEnd) {
-      
-    this.createNewTask(userTexst, generatedId, total, value);
-    this.clearValue();
-    this.saveToLocStor(userTexst, generatedId, total, value);
+    if (userTexst.length && dateStart && dateEnd) {
+
+      this.createNewTask(userTexst, generatedId, total, value);
+      this.clearValue();
+      this.saveToLocStor(userTexst, generatedId, total, value);
     } else {
 
       alert('Sorry, but You need to enter texst and Date')
     }
-    },
+  },
 
   transformDate: function (dateStart, dateEnd) {
     const startTime = new Date(dateStart).getTime();
     const endTime = new Date(dateEnd).getTime();
 
-    const currentTime = new Date(new Date().setHours(0,0,0,0)).getTime();
+    const currentTime = new Date(new Date().setHours(0, 0, 0, 0)).getTime();
 
-    const rezult =(endTime - startTime) / 1000/60/60/24 + 1;
-    const rest = (currentTime - startTime) / 1000/60/60/24;
-    const isExpired = ((currentTime - endTime) / 1000/60/60/24) > 0;
+    const rezult = (endTime - startTime) / 1000 / 60 / 60 / 24 + 1;
+    const rest = (currentTime - startTime) / 1000 / 60 / 60 / 24;
+    const isExpired = ((currentTime - endTime) / 1000 / 60 / 60 / 24) > 0;
 
     return { total: rezult, value: rest }
   },
 
-    createProgressBar: function (value, total, taskId) {
-      const taskIdName = '#' + taskId;
+  createProgressBar: function (value, total, taskId) {
+    const taskIdName = '#' + taskId;
 
-     jQuery(taskIdName)
+    jQuery(taskIdName)
       .progress({
-        duration : 200,
+        duration: 200,
         total,
         value,
-        text     : {
+        text: {
           active: '{value} of {total} done'
         }
       })
-    },
+  },
 
-    
-    clearValue: function () {
-      document.querySelector(".userTaskTexst").value = "";
-      const calendarValues = document.querySelectorAll(".calendarDateInput");
 
-      calendarValues.forEach(element => {
-        element.value = '';        
-      });
-    },
-    
-    generateId: function () {
-      let a = 'example';
-      let b = Math.floor(Math.random() * 10000);
-      return a + b;
-    },
+  clearValue: function () {
+    document.querySelector(".userTaskTexst").value = "";
+    const calendarValues = document.querySelectorAll(".calendarDateInput");
 
-    saveToLocStor: function (taskName, taskId, rezult, rest) {
-      const userData = localStorage.getItem('userData');
-      const newTask = {taskName, taskId, total: rezult, value: rest, status: ACTIVE_STATUS};
+    calendarValues.forEach(element => {
+      element.value = '';
+    });
+  },
 
-      if (!userData) {
-          localStorage.setItem('userData', JSON.stringify([newTask])) //SAVE TO LOCALsTORAGE
+  generateId: function () {
+    let a = 'example';
+    let b = Math.floor(Math.random() * 10000);
+    return a + b;
+  },
 
-      } else {
-          const parsedUserData = JSON.parse(userData);
-          parsedUserData.push(newTask);
-    
-          localStorage.setItem('userData', JSON.stringify(parsedUserData));
-    
-      }
-    },
+  saveToLocStor: function (taskName, taskId, rezult, rest) {
+    const userData = localStorage.getItem('userData');
+    const newTask = { taskName, taskId, total: rezult, value: rest, status: ACTIVE_STATUS };
 
-    currentStatus: 'active',
+    if (!userData) {
+      localStorage.setItem('userData', JSON.stringify([newTask])) //SAVE TO LOCALsTORAGE
 
-    changeStatusActive: function() {
-      this.currentStatus =  'active';
-      this.renderTaskList()
-    },
+    } else {
+      const parsedUserData = JSON.parse(userData);
+      parsedUserData.push(newTask);
 
-    changeStatusComplete: function() {
-      this.currentStatus =  'complete';
-      this.renderTaskList1()
-    },
+      localStorage.setItem('userData', JSON.stringify(parsedUserData));
 
-    renderTaskList: function () {
-      const userData = localStorage.getItem('userData');
-      
-      if (!userData) {
-        
-      } else {
-        document.querySelector(".userTask").innerHTML = "";
-        
-        const parsedUserData = JSON.parse(userData);
-        parsedUserData.forEach(element => {
-            if (element.status === this.currentStatus) {
-              const { taskName, taskId, total, value, status } = element;
-              this.createNewTask(taskName, taskId, total, value, status);
-          } 
-        });
-      }
-    
-    },
-
-    renderTaskList1: function () {
-      const userData = localStorage.getItem('userData');
-      
-      if (!userData) {
-      } else {
-        document.querySelector(".userTask").innerHTML = "";
-
-        const parsedUserData = JSON.parse(userData);
-        parsedUserData.forEach(element => {
-            if (element.status === 'complete') {
-              const { taskName } = element;
-              this.createNewTaskDeleted(taskName);
-          } 
-        });
-      }
-    
-    },
-
-    timerId: 0,
-
-    deleteTask: function (e) {
-      clearTimeout(this.timerId);
-      this.timerId = setTimeout(() => {
-          if (e.target.checked){
-            const deleteTaskId = e.target.dataset.taskId;
-            const userData = localStorage.getItem('userData');
-            const parsedUserData = JSON.parse(userData);
-      
-            const res = parsedUserData.map((item) => {
-              if(item.taskId === deleteTaskId){
-                item.status = COMPLETED_STATUS;
-              } 
-              
-              return item;
-            });
-      
-            localStorage.setItem('userData', JSON.stringify(res));
-            this.renderTaskList();
-          }  
-      }, 2000)
     }
+  },
+
+  currentStatus: 'active',
+
+  changeStatusActive: function () {
+    this.currentStatus = 'active';
+    this.renderTaskList()
+  },
+
+  changeStatusComplete: function () {
+    this.currentStatus = 'complete';
+    this.renderTaskList1()
+  },
+
+  renderTaskList: function () {
+    const userData = localStorage.getItem('userData');
+
+    if (!userData) {
+
+    } else {
+      document.querySelector(".userTask").innerHTML = "";
+
+      const parsedUserData = JSON.parse(userData);
+      parsedUserData.forEach(element => {
+        if (element.status === this.currentStatus) {
+          const { taskName, taskId, total, value, status } = element;
+          this.createNewTask(taskName, taskId, total, value, status);
+        }
+      });
+    }
+
+  },
+
+  renderTaskList1: function () {
+    const userData = localStorage.getItem('userData');
+
+    if (!userData) {
+    } else {
+      document.querySelector(".userTask").innerHTML = "";
+
+      const parsedUserData = JSON.parse(userData);
+      parsedUserData.forEach(element => {
+        if (element.status === 'complete') {
+          const { taskName } = element;
+          this.createNewTaskDeleted(taskName);
+        }
+      });
+    }
+
+  },
+
+  timerId: 0,
+
+  deleteTask: function (e) {
+    clearTimeout(this.timerId);
+    this.timerId = setTimeout(() => {
+      if (e.target.checked) {
+        const deleteTaskId = e.target.dataset.taskId;
+        const userData = localStorage.getItem('userData');
+        const parsedUserData = JSON.parse(userData);
+
+        const res = parsedUserData.map((item) => {
+          if (item.taskId === deleteTaskId) {
+            item.status = COMPLETED_STATUS;
+          }
+
+          return item;
+        });
+
+        localStorage.setItem('userData', JSON.stringify(res));
+        this.renderTaskList();
+      }
+    }, 2000)
+  }
 };
 
 
@@ -275,17 +275,17 @@ function createPopupModal() {
   const popupCloseIcon = document.querySelector('.close-popup');
 
   popupOpenLink.addEventListener('click', function (e) {
-      popupOpen();
+    popupOpen();
   })
 
   popupCloseIcon.addEventListener('click', function (e) {
-      popupClose();
+    popupClose();
   });
 
   document.addEventListener('keydown', function (e) {
-      if (e.key === 'Escape') {
-          popupClose();
-      }
+    if (e.key === 'Escape') {
+      popupClose();
+    }
   });
 }
 
@@ -294,15 +294,15 @@ function popupOpen() {
   const currentPopup = document.querySelector('#popupAchive');
 
   if (currentPopup) {
-      const popupActive = document.querySelector('#popupAchive.open');
+    const popupActive = document.querySelector('#popupAchive.open');
 
-      if (popupActive) {
-          popupClose();
-      } else {
-          bodyLock();
-      }
+    if (popupActive) {
+      popupClose();
+    } else {
+      bodyLock();
+    }
 
-      currentPopup.classList.add('open');
+    currentPopup.classList.add('open');
   }
 }
 
